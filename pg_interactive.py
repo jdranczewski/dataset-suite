aab94285c6015987982cd9697824e07fa489449f
jdranczewski <domdlapsa@gmail.com>
2024-02-08 15:09:44 +0000

    Add MIT and comments to files The whole repo is MIT, but the files are likely to migrate around so explicit acknowledgment is good.

--- pg_interactive.py
+++ pg_interactive.py
@@ -1,3 +1,30 @@
+"""
+Set of small tools and shims to make quick plotting with pyqtgraph easier.
+
+MIT License
+
+Copyright (c) 2023 Jakub Dranczewski
+Created as part of PhD work supported by the EU ITN EID project CORAL (GA no. 859841).
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 from pyqtgraph.Qt import QtCore, QtWidgets, QtWidgets
 import pyqtgraph as pg
 pg.setConfigOption('background', 'w')
@@ -5,6 +32,9 @@ pg.setConfigOption('foreground', 'k')
 
 
 class Control(QtWidgets.QWidget):
+    """
+    A slider with some sensible defaults. See get_pg_controls for easy creation.
+    """
     def __init__(self, name, values, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = name
@@ -39,6 +69,10 @@ class Control(QtWidgets.QWidget):
 
 
 def get_pg(row=1, col=1):
+    """
+    Produce a row x col grid of PlotItems. Returns the containing GraphicsLayoutWidget
+    and a flattened list of the PlotItems.
+    """
     w = pg.GraphicsLayoutWidget()
     ax = []
     for i in range(row):
@@ -50,6 +84,25 @@ def get_pg(row=1, col=1):
 
 
 def get_pg_controls(row=1, col=1, **controls):
+    """
+    Produce a row x col grid of PlotItems with Control sliders below.
+
+    The sliders are defined by providing keyword arguments with lists of values the slider
+    should go through. For example::
+    
+        temps = np.genfromtxt("temps.txt")
+        w, w_plot, ax, cdict = get_pg_controls(1, 1, temperatures=temps)
+        cdict['temperatures'].valueChanged.connect(update)
+    
+    will make a slider for the temperature list, and one can connect a function to the slider's
+    valueChanged Signal.
+
+    Returns:
+        w: the QWidget containing the GraphicsLayoutWidget and the Control sliders
+        w_plot: the GraphicsLayoutWidget of the plots
+        ax: a flattened list of the PlotItems
+        cdict: a dictionary of the Control sliders.
+    """
     w_plot, ax = get_pg(row, col)
 
     w = QtWidgets.QWidget()
